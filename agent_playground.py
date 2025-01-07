0372f8a8a72f625a5c5937777a28df69c73b1da6
Nitin <ntnbhat9@gmail.com>
2025-01-06 21:36:01 -0500

    added langgrapqh agentic ai

--- agent_playground.py
+++ agent_playground.py
@@ -1,10 +1,16 @@
 import uuid
 
-from langchain_core.messages import AIMessage
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.runnables import Runnable
 from langchain_core.tools import tool
 from langchain_google_vertexai import ChatVertexAI
 import requests
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, START, StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode
+
+from typing import Annotated, Literal, TypedDict
 
 from db_utils import get_chat_history
 from langchain_utils import get_rag_chain
@@ -57,26 +63,112 @@ llm = ChatVertexAI(
 # Using Agents with tools
 ############################################################################################################
 
+# tools = [dog_finder_tool, chat_tool]
+# llm_with_tools = llm.bind_tools(tools)
+# tool_map = {tool.name: tool for tool in tools}
+#
+#
+# def call_tools(msg: AIMessage) -> Runnable:
+#     """Simple sequential tool calling helper."""
+#     tool_calls = msg.tool_calls.copy()
+#     for tool_call in tool_calls:
+#         tool_call["output"] = tool_map[tool_call["name"]].invoke(tool_call["args"])
+#     return tool_calls
+#
+#
+# chain = llm_with_tools | call_tools
+#
+# res = chain.invoke("find dogs ?")
+# print("******** RESULT of prompt : find dogs ? ***********")
+# print(res[0]['output'])
+#
+# res = chain.invoke("who owns insurance module ?")
+# print("******** RESULT of prompt : who owns insurance module ? ***********")
+# print(res[0]['output'])
+
+
+############################################################################################################
+# Using Agents with langgraph
+############################################################################################################
 tools = [dog_finder_tool, chat_tool]
+
+tool_node  = ToolNode(tools)
+
 llm_with_tools = llm.bind_tools(tools)
-tool_map = {tool.name: tool for tool in tools}
+
+# Define the function that determines whether to continue or not
+def should_continue(state: MessagesState) -> Literal["tools", END]:
+    messages = state['messages']
+    last_message = messages[-1]
+    # If the LLM makes a tool call, then we route to the "tools" node
+    if last_message.tool_calls:
+        return "tools"
+    # Otherwise, we stop (reply to the user)
+    return END
 
 
-def call_tools(msg: AIMessage) -> Runnable:
-    """Simple sequential tool calling helper."""
-    tool_calls = msg.tool_calls.copy()
-    for tool_call in tool_calls:
-        tool_call["output"] = tool_map[tool_call["name"]].invoke(tool_call["args"])
-    return tool_calls
+# Define the function that calls the llm
+def call_llm(state: MessagesState):
+    messages = state['messages']
+    response = llm_with_tools.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+# Define a new graph
+workflow = StateGraph(MessagesState)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_llm)
+workflow.add_node("tools", tool_node)
 
 
-chain = llm_with_tools | call_tools
 
-res = chain.invoke("find dogs ?")
-print("******** RESULT of prompt : find dogs ? ***********")
-print(res[0]['output'])
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.add_edge(START, "agent")
 
-res = chain.invoke("who owns insurance module ?")
-print("******** RESULT of prompt : who owns insurance module ? ***********")
-print(res[0]['output'])
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+)
 
+# We now add a normal edge from `tools` to `agent`.
+# This means that after `tools` is called, `agent` node is called next.
+workflow.add_edge("tools", 'agent')
+
+
+# Initialize memory to persist state between graph runs
+checkpointer = MemorySaver()
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable.
+# Note that we're (optionally) passing the memory when compiling the graph
+app = workflow.compile(checkpointer=checkpointer)
+
+# Use the Runnable
+final_state = app.invoke(
+    {"messages": [HumanMessage(content="who owns insurance module ?")]},
+    config={"configurable": {"thread_id": 42}}
+)
+
+print(final_state["messages"][-1].content)
+
+final_state = app.invoke(
+    {"messages": [HumanMessage(content="how many tables are there in this module ?")]},
+    config={"configurable": {"thread_id": 42}}
+)
+
+print(final_state["messages"][-1].content)
+
+
+final_state = app.invoke(
+    {"messages": [HumanMessage(content="can you find some dogs for me ?")]},
+    config={"configurable": {"thread_id": 42}}
+)
+
+print(final_state["messages"][-1].content)
